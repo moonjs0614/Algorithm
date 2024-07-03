0030c28028851b5d234e6a9b8b024519349ba7b0
JunSeok <moonjs0614@naver.com>
2024-07-03 14:43:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int n;
+long long dp[1001];
+
+void solution()
+{
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+	}
+
+	cout << dp[n];
+}
+
+int main() {
+	//freopen("sample_input.txt", "r", stdin);
+	cin >> n;
+
+	solution();
+}
